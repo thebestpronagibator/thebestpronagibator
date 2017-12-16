833d4f4d78d8345de5cdf2ca5d9111379fceb7e9
thebestpronagibator <34607090+thebestpronagibator@users.noreply.github.com>
2017-12-16 21:17:00 +0200

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,3 +125,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = '/media/'
+ALLOWED_HOSTS = ('thebestpronagibator.pythonanywhere.com', )
